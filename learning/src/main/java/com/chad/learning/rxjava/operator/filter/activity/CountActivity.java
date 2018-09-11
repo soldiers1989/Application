a83f14af8fb771a3e72fd4c323154af594436380
jxzhang <zhangjunxiang1995@163.com>
2018-09-11 19:38:33 +0800

    Reason:tian jia take cao zuo fu

--- learning/src/main/java/com/chad/learning/rxjava/operator/filter/activity/CountActivity.java
+++ learning/src/main/java/com/chad/learning/rxjava/operator/filter/activity/CountActivity.java
@@ -1,9 +1,116 @@
 package com.chad.learning.rxjava.operator.filter.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+
+import com.chad.learning.R;
+import com.chad.learning.parent.base.BaseAppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 /**
  * 根据指定事件数量过滤事件
  */
-public class CountActivity extends AppCompatActivity {
+public class CountActivity extends BaseAppCompatActivity {
+
+    @BindView(R.id.text_content)
+    AppCompatTextView mTextContent;
+
+    private String mContent;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_rx_java_operator_filter_count;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    // take()过滤操作符，指定观察者最多能接收到的事件数量
+    @OnClick(R.id.btn_take)
+    public void onTakeClick() {
+        mContent = "take \n";
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onNext(4);
+                emitter.onNext(5);
+            }
+        }).take(3)
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        mContent += "onNext = " + integer;
+                        mTextContent.setText(mContent);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    // takeLast()过滤操作符,指定观察者只能接收到被观察者发送的最后几个事件
+    @OnClick(R.id.btn_take_last)
+    public void onTakeLastClick() {
+        mContent = "takeLast \n";
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+
+            }
+        }).takeLast(3)
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        mContent += "onNext = " + integer;
+                        mTextContent.setText(mContent);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 }
